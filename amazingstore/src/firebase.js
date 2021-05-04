67d8b3eae275a19a7473cf873d63e36ad246f45a
Daniel <danielliu1824@gmail.com>
2021-05-04 16:28:13 -0700

    Initial commit

---
+++ amazingstore/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBgM743UeBJBqEMb1LB_5PS4yqSenRa8N8",
+  authDomain: "amazingstore-2e894.firebaseapp.com",
+  projectId: "amazingstore-2e894",
+  storageBucket: "amazingstore-2e894.appspot.com",
+  messagingSenderId: "731224474864",
+  appId: "1:731224474864:web:95cf485cf846f0993c6e35",
+  measurementId: "G-2TQ6QVSQCE"
+};
