52ec5e4f5fb7f0b2c85ce811de7d539e1810f4f0
Daniel <danielliu1824@gmail.com>
2021-05-06 18:49:21 -0700

    Added header icons, and click functionality to github icon. More css cleanup

--- amazingstore/src/Header.js
+++ amazingstore/src/Header.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import './Header.css';
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import GitHubIcon from '@material-ui/icons/GitHub';
+
 
 export default function Header() {
+
+  const handleClick = () => {
+    window.location.href="https://github.com/DanielL158/AmazingStore/tree/master/amazingstore"
+  }
+
   return (
     <div className='header'>
       <img
       className='header_logo'
-      src='http://pngimg.com/uploads/amazon/amazon_PNG11.png' />
+      src='http://pngimg.com/uploads/amazon/amazon_PNG11.png' alt='' />
 
       <div className='header_search'>
-        <input className='header_searchInput' type='text' />
+        <input
+        className='header_searchInput'
+        type='text' />
+        <SearchIcon
+        className='header_searchIcon' />
 
         <div className='header_nav'>
           <div className='header_option'>
@@ -32,6 +45,15 @@ export default function Header() {
             <span
             className='header_optionLineTwo'>Prime</span>
           </div>
+
+          <div className='header_optionGitHub'>
+          <GitHubIcon onClick={handleClick}/>
+          </div>
+          <div className='header_optionBasket'>
+            <ShoppingBasketIcon />
+            <span className='header_optionLineTwo header_basketCount'>0</span>
+          </div>
+
         </div>
       </div>
     </div>
