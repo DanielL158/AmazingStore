7f4d8cad8579cf7ba5f75a6bc09d1f370c709b80
Daniel <danielliu1824@gmail.com>
2021-05-11 20:45:03 -0700

    Added static product + Product CSS

--- amazingstore/src/Product.js
+++ amazingstore/src/Product.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import './Product.css';
 
-function Product () {
+export default function Product () {
   return (
     <div className='product'>
+      <div className='product_info'>
+        <p>The lean start up</p>
+        <p className='product_price'>
+          <small>$</small>
+          <strong>19.99</strong>
+        </p>
+        <div  className='product_rating'>
+            <p>🌟</p>
+            <p>🌟</p>
+            <p>🌟</p>
+        </div>
+      </div>
 
+      <img src='https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg'
+      alt='' />
+
+      <button>Add to Basket</button>
     </div>
   )
 }
