998b115d42fe056032dccc386b2172939912fb45
Daniel <danielliu1824@gmail.com>
2021-05-17 04:46:29 -0700

    Firebase login and signup functionality

--- amazingstore/src/firebase.js
+++ amazingstore/src/firebase.js
@@ -1,4 +1,5 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from 'firebase';
+
 const firebaseConfig = {
   apiKey: "AIzaSyBgM743UeBJBqEMb1LB_5PS4yqSenRa8N8",
   authDomain: "amazingstore-2e894.firebaseapp.com",
@@ -8,3 +9,10 @@ const firebaseConfig = {
   appId: "1:731224474864:web:95cf485cf846f0993c6e35",
   measurementId: "G-2TQ6QVSQCE"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
